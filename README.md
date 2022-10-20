070f5738e15a86885c400484e57a76b57514c94f
Breno Viana <bmfviana@gmail.com>
2022-10-20 16:27:07 -0300

    adiciona descrição no README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # py-bdd-context
+
+Biblioteca com Context Manager para testes Behavior-driven development (BDD).
---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+    description = "Flake do py-bdd-context";
+
+    inputs = {
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+
+        pkgsAllowUnfree = import nixpkgs {
+          system = "x86_64-linux";
+          config = { allowUnfree = true; };
+        };
+
+        config = {
+          projectDir = ./.;
+        };
+
+      in
+      {
+
+        devShell = pkgsAllowUnfree.mkShell {
+          buildInputs = with pkgsAllowUnfree; [
+            gnumake
+            poetry
+            python3
+          ];
+
+          shellHook = ''
+            # TODO: documentar esse comportamento,
+            # devo abrir issue no github do nixpkgs
+            export TMPDIR=/tmp
+
+            # O PyCharm ativa por padrão o ambiente virtual.
+            # Esse comando cria o .venv caso não exista e
+            # ativa o .venv.
+            # Notar que pode haver dessincronia por conta de
+            # um .venv desatualizado.
+            test -f .venv/bin/activate || make poetry.install
+            source .venv/bin/activate
+            # Se não existir cria o .env com valores padrão
+            if ! test -f .env; then
+              make config.env
+            fi
+            echo "Entering the nix devShell no income back"
+          '';
+        };
+      }
+    );
+}
