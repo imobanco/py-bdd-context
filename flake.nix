a9883681e7f8d1bce340dfabfe4975f6999ca7b4
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-10-21 16:07:22 -0300

    Remove make config.env do shellHook

--- flake.nix
+++ flake.nix
@@ -40,10 +40,7 @@
             # um .venv desatualizado.
             test -f .venv/bin/activate || make poetry.install
             source .venv/bin/activate
-            # Se não existir cria o .env com valores padrão
-            if ! test -f .env; then
-              make config.env
-            fi
+
             echo "Entering the nix devShell no py-bdd-context"
           '';
         };
