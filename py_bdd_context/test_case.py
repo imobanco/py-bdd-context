d456c82cc2e6f3b3271edee8a80f55e98f063a9a
Ariane Silveira <arianesilveira97@hotmail.com>
2022-10-31 17:42:03 -0300

    adiciona docstring em test_case

--- py_bdd_context/test_case.py
+++ py_bdd_context/test_case.py
@@ -10,15 +10,24 @@ class BDDContextTestCase(TestCase):
     then = then
 
     def testDescriptionInfo(self):
+        """
+        :return: test path and line
+        """
         test_lineno = TestFileHelper().get_test_method_line_number_for_test(
             self, self._testMethodName
         )
         return ["", f"{test_lineno} | teste"]
 
     def bddDescriptionInfo(self):
+        """
+        :return:  test description
+        """
         return getattr(self, "_aditional_bdd_description_infos", [])
 
     def shortDescription(self):
+        """
+        :return: test description, path and line
+        """
         original_description = super().shortDescription()
 
         if not isinstance(original_description, str):
