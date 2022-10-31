85b3857e898a7bd6a90d943ea295cbe59955dd14
Rodrigo Castro <rodrigondec@gmail.com>
2022-10-31 13:05:51 -0300

    adiciona linha do teste

--- py_bdd_context/test_case.py
+++ py_bdd_context/test_case.py
@@ -1,12 +1,19 @@
 from unittest import TestCase
 
 from py_bdd_context.bdd_context import dado, entao, quando
+from py_bdd_context.test_file_helper import TestFileHelper
 
 
 class BDDContextTestCase(TestCase):
     dado = dado
     quando = quando
     entao = entao
+
+    def testDescriptionInfo(self):
+        test_lineno = TestFileHelper().get_test_method_line_number_for_test(
+            self, self._testMethodName
+        )
+        return ["", f"{test_lineno} | teste"]
 
     def bddDescriptionInfo(self):
         return getattr(self, "_aditional_bdd_description_infos", [])
@@ -17,6 +24,6 @@ class BDDContextTestCase(TestCase):
         if not isinstance(original_description, str):
             original_description = ""
 
-        infos = [*self.bddDescriptionInfo()]
+        infos = [*self.testDescriptionInfo(), *self.bddDescriptionInfo()]
 
         return original_description + "\n".join(infos)
