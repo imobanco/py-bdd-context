50b57d39243d984282ef565795343584c577a204
Rodrigo Castro <rodrigondec@gmail.com>
2022-10-31 10:27:57 -0300

    adiciona exemplo de utilização da lib

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Biblioteca com Context Manager para facilitar os testes de Behavior Driven Development (BDD).
 
 Essa biblioteca irá te ajudar a organizar os seus testes!
+
+## Instalação
+
+A lib se encontra no pypi ??
+
+```shell
+pip install py-bdd-context
+```
+
+## Utilizando
+Existem exemplos de como utilizar a lib na pasta `examples`.
+
---
+++ examples/sun_rising.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from py_bdd_context import BDDContextTestCase
+
+
+def rise_sun(hour: int):
+    if hour > 6:
+        return False
+    return True
+
+
+class MyTestCase(BDDContextTestCase, TestCase):
+    def test_rise_sun_before_6(self):
+        with self.dado(
+            """
+            - são 5h da manhã
+            """
+        ):
+            current_hour = 5
+
+        with self.quando(
+            """
+            - o sol nascer
+            """
+        ):
+            result = rise_sun(current_hour)
+
+        with self.entao(
+            """
+            - o sol deve ter nascido corretamente 
+            """
+        ):
+            self.assertEqual(result, True)
+
+    def test_rise_sun_after_6(self):
+        with self.dado(
+            """
+            - são 6h da manhã
+            """
+        ):
+            current_hour = 6
+
+        with self.quando(
+            """
+            - o sol nascer
+            """
+        ):
+            result = rise_sun(current_hour)
+
+        with self.entao(
+            """
+            - o sol não deve ter nascido 
+            """
+        ):
+            self.assertEqual(result, False)
