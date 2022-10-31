1f45a76e01b60dc10560a4ab5458fe00a8af5487
Ariane Silveira <arianesilveira97@hotmail.com>
2022-10-31 17:41:42 -0300

    adiciona docstring em test_file_helper

--- py_bdd_context/test_file_helper.py
+++ py_bdd_context/test_file_helper.py
@@ -4,12 +4,21 @@ import os
 
 class TestFileHelper:
     def get_file_path_for_test(self, test):
+        """
+        :param test:
+        :return:file path for test
+        """
         test_class = test.__class__
         path = os.path.relpath(inspect.getfile(test_class))
 
         return f"{path}"
 
     def get_test_method_line_number_for_test(self, test, test_method_name):
+        """
+        :param test:
+        :param test_method_name:
+        :return: test path and line
+        """
         test_path = self.get_file_path_for_test(test)
 
         test_class = test.__class__
@@ -24,6 +33,11 @@ class TestFileHelper:
         return f"{test_path}:{lineno}"
 
     def get_exception_line_number_for_test(self, test, exc_tb):
+        """
+        :param test:
+        :param exc_tb:
+        :return: exception line
+        """
         test_path = self.get_file_path_for_test(test)
         lineno = exc_tb.tb_lineno
         return f"{test_path}:{lineno}"
